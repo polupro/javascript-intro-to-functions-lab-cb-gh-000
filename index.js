d8e1b2b30048c8869770d6e47db5d3275a7c3145
Sergiy  Tkachuk <awertg@mail.ua>
2017-09-03 13:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function shout(string) {
-  console.log(string.toUpperCase());
-}
 
-function whisper(string) {
-  console.log(string.toLowerCase());
-}
